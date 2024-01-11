4ee7bb56687ac05d6beb3dc1746c177e1fd28003
I Bo <ilya112358@gmail.com>
2024-01-11 21:27:03 +0000

    Size and backcolor

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,18 +16,24 @@ import pandas as pd
 #     print(message, flush=True, end='')
 
 df = pd.read_csv('data.csv')
-p = figure(title='Right Ankle Angles', x_axis_label='Gait cycle, %', y_axis_label='Degrees')
+p = figure(title='Right Ankle Angles', 
+           x_axis_label='Gait cycle, %', 
+           y_axis_label='Degrees', 
+           height=400,
+           width=600)
 p.title.align = "center"
 p.title.text_font_size = "25px"
-p.toolbar.active_drag = p.select_one(PanTool)
+p.border_fill_color = "seashell"
+# p.toolbar.active_drag = p.select_one(PanTool)
+p.toolbar.active_drag = None
 p.toolbar.active_scroll = p.select_one(WheelZoomTool)
 colors = ['red', 'green', 'blue', 'orange', 'black']
 lines, labels = [], []
-for walk in range(1, len(df.columns)):
-    line = p.line('Gait cycle', df.columns[walk], source=ColumnDataSource(df), color=colors[walk-1])
+for col in range(1, len(df.columns)-1):
+    line = p.line('Gait cycle', df.columns[col], source=ColumnDataSource(df), color=colors[col-1])
     lines.append(line)
-    labels.append((df.columns[walk], [line]))
-lines[-1].glyph.line_width = 2
+    labels.append((df.columns[col], [line]))
+# lines[-1].glyph.line_width = 2
 legend = Legend(items=labels, location="center")
 legend.orientation = "horizontal"
 legend.border_line_color = "black"
@@ -35,4 +41,4 @@ p.add_layout(legend, 'above')
 
 st.title("Check Bokeh Library")
 st.text("before using it")
-components.html(file_html(p, 'cdn', ), height=600)
+components.html(file_html(p, 'cdn', ), height=400, width=600)
